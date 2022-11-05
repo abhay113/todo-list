ddfa1180092460010447b85754970fc6cdc0af92
Abhay <abhaychavan346@gmail.com>
2022-11-05 12:49:16 +0530

    initial commit

---
+++ src/MyComponents/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+           <p>
+             This is a simple todo list project build by me.
+             In this project I have used the React Framework. 
+             With react we can create single page applications.
+            </p>
+        </div>
+    )
+}
